f184794b46419a4c11c58599599eb14182a53a6a
Ludwig Krause <ludwigkrause@t-online.de>
2019-10-25 15:12:31 +0200

    scraper_mailer.sh

--- scraper_mailer.sh
+++ scraper_mailer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 while sleep 5; do 
 
-cd wg_gesucht && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "WG_GESUCHT $(date) New Apartment" luimuiluimui2@gmail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd .. && 
+cd wg_gesucht && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "WG_GESUCHT $(date) New Apartment" example@mail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd .. && 
 
-cd ebay_kleinanzeigen && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "EBAY-KA $(date) New Apartment" luimuiluimui2@gmail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd .. &&
+cd ebay_kleinanzeigen && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "EBAY-KA $(date) New Apartment" example@mail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd .. &&
 
-cd immoscout24 && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "IMMOSCOUT24 $(date) New Apartment" luimuiluimui2@gmail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd ..;
+cd immoscout24 && python scraper.py > body.tmp && cat body.tmp | ifne mail -s "IMMOSCOUT24 $(date) New Apartment" example@mail.com | cat body.tmp | ifne  printf "$(date +"%T")\n" && cat body.tmp && cd ..;
 
 done
 
